747e401faa50aef08ca1caf6fbb1ec04adc5f832
Jerry <thrforever@126.com>
2017-07-11 14:31:24 +0800

    更新图片滚动 1. 图片停留时长

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,59 +1,71 @@
-import { Component , ViewChild} from '@angular/core';
-import { NavController ,App,Slides} from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {NavController, App, Slides} from 'ionic-angular';
 
-import { Blsj } from '../jumpPages/blsj/blsj';
-import { NurseClass } from '../jumpPages/nurse-class/nurse-class';
-import { Score } from '../hospital/score/score';
-import { Chat } from '../../pages/contact/chat/chat';
-import { Notice } from '../../pages/common/notice/notice';
+import {Blsj} from '../jumpPages/blsj/blsj';
+import {NurseClass} from '../jumpPages/nurse-class/nurse-class';
+import {Score} from '../hospital/score/score';
+import {Chat} from '../../pages/contact/chat/chat';
+import {Notice} from '../../pages/common/notice/notice';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  toUser:Object;
-  @ViewChild('mainSlide') mainSlides: Slides; 
+  toUser: Object;
+  @ViewChild('mainSlide') mainSlides: Slides;
   @ViewChild('noticeSlide') noticeSlides: Slides;
-  constructor(public navCtrl: NavController,private app:App) {
-  this.toUser = {
-      toUserId:'210000198410281948',
-      toUserName:'喵喵'
+
+  constructor(public navCtrl: NavController, private app: App) {
+    this.toUser = {
+      toUserId: '210000198410281948',
+      toUserName: '喵喵'
     }
 
 
   }
 
-  ionViewDidEnter(){ 
+  ionViewDidEnter() {
     // this.mainSlides.update();
     this.mainSlides.autoplayDisableOnInteraction = false;
-    this.mainSlides.autoplay=1;
-    this.mainSlides.startAutoplay();
+    this.mainSlides.autoplay = 1;
+    // this.mainSlides.startAutoplay();
     // this.mainSlides.slideTo(2, 500);
-    
-    this.noticeSlides.startAutoplay(); 
+    var that = this;
+    setInterval(function () {
+      that.mainSlides.slideNext(500);
+    }, 5500);
+
+    this.noticeSlides.startAutoplay();
     this.noticeSlides.autoplayDisableOnInteraction = false;
-  } 
-  ionViewDidLeave(){   
-    this.mainSlides.stopAutoplay(); 
-    this.noticeSlides.stopAutoplay(); 
   }
-  goBlsj(){
+
+  ionViewDidLeave() {
+    this.mainSlides.stopAutoplay();
+    this.noticeSlides.stopAutoplay();
+  }
+
+  goBlsj() {
     this.app.getRootNav().push(Blsj);
   }
-  goNurseClass(){
+
+  goNurseClass() {
     this.app.getRootNav().push(NurseClass);
   }
-  goScore(){
+
+  goScore() {
     this.navCtrl.push(Score);
   }
-  goContact(){
+
+  goContact() {
     this.navCtrl.parent.select(1);
   }
-  goChat(){
-    this.app.getRootNav().push(Chat,this.toUser);
+
+  goChat() {
+    this.app.getRootNav().push(Chat, this.toUser);
   }
-  goNotice(){
+
+  goNotice() {
     this.app.getRootNav().push(Notice);
   }
 
